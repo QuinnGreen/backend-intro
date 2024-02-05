b07bcd04d9705b29c56aff81ca48961416065a28
quinn <williammsullivansabre@gmail.com>
2024-02-05 17:44:35 +0000

    extra pages and navigation

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get("/health", (req, res) => {
-  res.send("healthy");
-});
+app.use("/example", express.static("example"));
+
+app.use("/Lorem", express.static("Lorem"));
+
+// app.get("/health", (req, res) => {
+//   res.send("healthy");
+// });
 
 app.listen(5001, () => {
   console.log("server is listening on port 5001");
